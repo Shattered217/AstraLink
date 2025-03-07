1613ac33e3ac05ab07b437266d8a7eabd59e4986
Shattered? <102911315+Shattered217@users.noreply.github.com>
2025-03-07 21:09:32 +0800

    Update audio.py

--- audio.py
+++ audio.py
@@ -6,10 +6,11 @@ sck_pin = Pin(18)
 ws_pin = Pin(8)
 sd_pin = Pin(17)
 
-audio_out = I2S(1, sck=sck_pin, ws=ws_pin, sd=sd_pin, mode=I2S.TX, bits=16, format=I2S.MONO, rate=44100, ibuf=40000)
-
 async def play_audio(url):
     try:
+        # 初始化 I2S
+        audio_out = I2S(1, sck=sck_pin, ws=ws_pin, sd=sd_pin, mode=I2S.TX, bits=16, format=I2S.MONO, rate=44100, ibuf=40000)
+        
         response = urequests.get(url)
         response.raw.read(44)
         print("音频开始播放...")
@@ -27,3 +28,4 @@ async def play_audio(url):
         print("播放音频时发生异常: ", e)
     finally:
         response.close()
+
