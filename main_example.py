bffe7d8c8f25d24009f75f72424d02da1b9f8372
Shattered? <102911315+Shattered217@users.noreply.github.com>
2025-03-07 21:09:06 +0800

    Update main_example.py

--- main_example.py
+++ main_example.py
@@ -4,6 +4,7 @@ from audio import play_audio
 import uasyncio as asyncio
 from baidu_audio import *
 from mic import *
+from silicon_deepseek import *
 
 async def audio_handler():
     print("检测到触发, 播放音频...")
@@ -15,35 +16,32 @@ async def audio_handler():
 def trigger_callback():
     """回调函数，执行音频播放"""
     await audio_handler() 
-    print("音频播放完成")
-    
-    # 创建 AudioRecorder 实例
+    print("开始录音")
+                                      
     recorder = AudioRecorder()
-    # 开始录音
-    recorder.record_audio(3, "recording.wav")
-    # 关闭 I2S 接口
+    recorder.record_audio(5, "recording.wav")
     recorder.deinit()
+    print("录音结束")
     
     try:
-        # 语音识别示例：从 PCM 文件识别文字
         recognized_text = recongize("recording.wav")
         print(f"识别结果: {recognized_text}")
     except ValueError as e:
         print(f"语音识别错误: {e}")
     
+    ai_text = ask_question(recognized_text)
+    
     try:
-        # 语音合成示例：将文本转为语音并播放
-        speech_tts(apikey, sercretkey, recognized_text)
+        speech_tts(apikey, sercretkey, ai_text)
     except Exception as e:
         print(f"语音合成错误: {e}")
 
 async def main():
     print("Welcome to MicroPython!")
-    
+     
     # 注册触发回调
     active.register_trigger_callback(trigger_callback)
     
-    # 连接WiFi
     await do_connect()
     
     # 启动触发监控任务
