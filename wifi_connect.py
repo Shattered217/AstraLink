ff038cf55887617e1cef888bdc8ebbb3f2a0b560
Shattered? <102911315+Shattered217@users.noreply.github.com>
2025-03-10 17:18:54 +0800

    Update wifi_connect.py

--- wifi_connect.py
+++ wifi_connect.py
@@ -2,12 +2,13 @@ import network
 import time
 import uasyncio as asyncio
 
-async def do_connect():
+async def do_connect(WIFI_SSID, WIFI_PASSWORD):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
         print("connecting to network...")
-        wlan.connect('iot', '1030210302')
+        print("WiFi名称: "+WIFI_SSID+"  WiFi密码: "+WIFI_PASSWORD)
+        wlan.connect(WIFI_SSID, WIFI_PASSWORD)
         while not wlan.isconnected():
             print("正在连接...")
             await asyncio.sleep(1)  # 非阻塞等待
